d9cc376f7f6442517afc56944970ec251981ba57
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:15:59 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,19 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "id" {
+  value       = module.acm.id
+  description = "The ID of the certificate"
+}
+
+output "arn" {
+  value       = module.acm.arn
+  description = "The ARN of the certificate"
+}
+
+output "domain_validation_options" {
+  value       = module.acm.domain_validation_options
+  description = "CNAME records that are added to the DNS zone to complete certificate validation"
+}
+
+output "domain_name" {
+  value       = local.enabled ? local.domain_name : null
+  description = "Certificate domain name"
 }
