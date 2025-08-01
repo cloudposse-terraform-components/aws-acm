c68326ef00a2fa1b2bca2da859fbd9fe6e96ab0b
Marat Bakeev <hawara@gmail.com>
2025-08-02 00:24:46 +1200

    chore: run terraform fmt

--- src/main.tf
+++ src/main.tf
@@ -25,7 +25,7 @@ module "acm" {
   version = "0.18.1"
 
   certificate_authority_arn         = local.private_ca_enabled ? module.private_ca[0].outputs.private_ca[var.certificate_authority_component_key].certificate_authority.arn : null
-  validation_method                 = local.private_ca_enabled ? null : var.validation_method  
+  validation_method                 = local.private_ca_enabled ? null : var.validation_method
   domain_name                       = local.domain_name
   process_domain_validation_options = var.process_domain_validation_options
   certificate_export                = var.certificate_export
